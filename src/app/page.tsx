2d008c5363e07de003788e3cf9734b5b6182d867
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-10-14 16:35:18 +0300

    Include the about me page in the main page entry point

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from "./components/navbar/Navbar";
+import AboutAlosa from "./sections/about/About";
 import Hero from "./sections/hero/Hero";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Navbar />
       <Hero />
+      <AboutAlosa />
     </>
   )
 }
