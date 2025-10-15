d65b144d0642b8271a74e0440d0ecc5c0df459aa
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-10-15 10:44:32 +0300

    Setup the My services section and added the component to the main project entry point

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/navbar/Navbar";
 import AboutAlosa from "./sections/about/About";
 import Hero from "./sections/hero/Hero";
+import MyServices from "./sections/services/Services";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <AboutAlosa />
+      <MyServices />
     </>
   )
 }
---
+++ src/app/sections/services/Services.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+const MyServices = () => {
+    return (
+        <section className="bg-pure-white py-16">
+
+        </section>
+    )
+}
+
+export default MyServices;
