7489f6c6f5d978982ffb684dbc62ad24b0060de1
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-10-16 15:49:51 +0300

    Update included:   - Included the my services event caricatures component to the main services component   - Added the image to use in the my services Event caricature section

--- src/app/sections/services/Services.tsx
+++ src/app/sections/services/Services.tsx
@@ -1,11 +1,13 @@
 "use client"
 
+import EventCaricature from "./sections/EventCaricature";
 import MyServicesTopBar from "./sections/ServicesTopBar";
 
 const MyServices = () => {
     return (
         <section className="bg-pure-white px-[5%] py-16">
           <MyServicesTopBar />
+          <EventCaricature />
         </section>
     )
 }
