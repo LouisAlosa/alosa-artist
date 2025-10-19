1824679f6934cbe9def9f3ecf9a949004f30339e
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-10-19 17:15:00 +0300

    Create and style the gallery digital caricatures section for mobile, tablet and desktop views

--- src/app/sections/gallery/Gallery.tsx
+++ src/app/sections/gallery/Gallery.tsx
@@ -2,12 +2,16 @@
 
 import { useState } from "react";
 import GalleryTopBar from "./sections/GalleryTopBar";
+import DigitalCaricatures from "./sections/DigitalCaricatures";
 
 const Gallery = () => {
     const [activeGallery, setActiveGallery] = useState("Digital Caricatures")
     return (
-        <section className="">
+        <section className="bg-pure-white px-[5%] py-16">
             <GalleryTopBar activeGallery={activeGallery} setActiveGallery={setActiveGallery} />
+
+            {/* Gallery section toggled */}
+            <DigitalCaricatures />
         </section>
     )
 }
