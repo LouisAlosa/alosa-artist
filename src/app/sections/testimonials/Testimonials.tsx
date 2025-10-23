516e46a4d0c2e1dc9a355f7f1ddf8d0ec363a039
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-10-23 16:17:37 +0300

    Add the Testimonials section adding the topbar section as well for this section

---
+++ src/app/sections/testimonials/Testimonials.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import TestimonialsTopBar from "./sections/TestimonialsTopar";
+
+const Testimonials = () => {
+  return (
+    <section className="bg-pure-white px-[5%] py-16">
+      <TestimonialsTopBar />
+    </section>
+  )
+}
+
+export default Testimonials;
---
+++ src/app/sections/testimonials/sections/TestimonialsTopar.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+const TestimonialsTopBar = () => {
+  return (
+    <section className="bg-pure-white w-full mb-6">
+      {/* Testimonials intro section */}
+      <div className="flex flex-col items-center justify-center max-w-[800px] mx-auto text-center">
+        {/* Heading */}
+        <h2 className="font-fredoka font-[400] text-dark-charcoal text-[36px] mb-4">
+          Testimonials
+        </h2>
+
+        {/* Divider */}
+        <div className="bg-silver-gray w-[80px] h-[6px] rounded-sm mb-6" />
+      </div>
+    </section>
+  )
+}
+
+export default TestimonialsTopBar;
