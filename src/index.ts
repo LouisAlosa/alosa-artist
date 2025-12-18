b8b6151f93e0aba8436b28959d33ec9f7e670ad9
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-12-18 15:48:29 +0300

    Un used files

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request: Request, env: any, ctx: ExecutionContext) {
-    return new Response("Hello from Alosa Worker!", {
-      headers: { "content-type": "text/plain" },
-    });
-  },
-};
